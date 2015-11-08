9b06c367a19e1f6168560b3502cab2b1aa9ee68b
caedmonz <862172548@qq.com>
2015-11-08 19:36:52 +0800

    add a simple Rpc

---
+++ Remote/Proxy.md
@@ -0,0 +1,4 @@
+##远程过程调用
+使用到基本思想动态代理
+- Proxy
+- InvocationHandler
---
+++ Remote/SimpleRPC/RpcProvider.java
@@ -0,0 +1,6 @@
+public class RpcProvider{
+	public static void main(String[] args)throws Exception {
+		HelloService service = new HelloServiceImpl();
+		RpcFramework.export(service, 1234);
+	}
+}
